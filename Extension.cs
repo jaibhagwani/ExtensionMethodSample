666402887cd2cb48caa9ef07231149154792f6b9
Jai <jaikumar.bhagwani@gmail.com>
2019-07-09 16:46:07 +0530

    Initial Commit

---
+++ Extension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodSample
+{
+    public static class Extension
+    {
+        public static string RemoveSpace(this string str)
+        {
+            return str.Replace(" ", string.Empty);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExtensionMethodSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter String to remove space: ");
+            string str = Console.ReadLine();
+            Console.WriteLine("Output: " + str.RemoveSpace());
+
+            Console.ReadLine();
+        }
+    }
+}
